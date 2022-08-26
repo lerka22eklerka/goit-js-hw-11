48ed117fa35b16de6533dcd80dc4fe2aac69b28c
lerka22eklerka <94803605+lerka22eklerka@users.noreply.github.com>
2022-08-26 14:22:32 +0300

    refresh

--- src/index.js
+++ src/index.js
@@ -18,28 +18,34 @@ let searchQuery = '';
 let page = 1;
 let images = [];
 let totalPages = 0;
+let lightbox;
+
 loadMoreBtn.classList.add('invisible');
+
 function onSearchQuery(event) {
     event.preventDefault();
-    searchQuery = event.target.elements.searchQuery.value;
-     
+    searchQuery = event.target.elements.searchQuery.value; 
     // console.log(searchQuery);
     fetchByQuery(searchQuery, page).then(({ data }) => {
         images = data.hits;
-        totalPages = data.totalHits / 40;
+        // totalPages = data.totalHits / 40;
         console.log(images);
-        if (images.length === 0 && searchQuery === '') {
-            Notiflix.Notify.failure(
-              'Sorry, there are no images matching your search query. Please try again.'
+      if (images.length === 0 || searchQuery === '') {
+          console.log(data.totalHits);
+          Notiflix.Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
           );
-          loadMoreBtn.classList.add('invisible');
-            return;
+      } else {
+        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+        galleryRef.innerHTML = '';
+        onRender(images);
+       lightbox = new SimpleLightbox('.gallery div a', {
+          captionDelay: 250,
+        });
+        loadMoreBtn.classList.remove('invisible');
         }
-      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-      galleryRef.innerHTML = '';
-      onRender(images);
-      loadMoreBtn.classList.remove('invisible');
-        if (totalPages === 14) {
+      
+        if (images.length < 40) {
           Notiflix.Notify.info(
             "We're sorry, but you've reached the end of search results."
           );
@@ -53,19 +59,17 @@ function onSearchQuery(event) {
 function onRender(images) {
   
     const gallery = images.map(image => markUpImg(image)).join('');
-    galleryRef.insertAdjacentHTML('beforeend', gallery);
-    var lightbox = new SimpleLightbox('.gallery div a', { captionDelay: 250 });
-
+  galleryRef.insertAdjacentHTML('beforeend', gallery);
+  
 }
 
 loadMoreBtn.addEventListener('click', onLoadMore);
 
-async function onLoadMore() {
+function onLoadMore() {
   try {
      page += 1;
-   await fetchByQuery(searchQuery, page).then(({ data }) => {
+fetchByQuery(searchQuery, page).then(({ data }) => {
         images = data.hits;
-        // totalPages = data.totalHits / 40;
      console.log(images);
      if (images.length === 0) {
         Notiflix.Notify.info(
@@ -73,8 +77,12 @@ async function onLoadMore() {
        );
        loadMoreBtn.classList.add('invisible');
      }
-    })
+})
+  
     onRender(images);
+  lightbox = new SimpleLightbox('.gallery div a', { captionDelay: 250 });
+    lightbox.refresh();
+     
   } catch (error) {
     if (error.name === 'AxiosError') {
       Notiflix.Notify.info(
